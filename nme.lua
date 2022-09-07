110ae5383df89fddcbd709dd4bb2eb84a918ea5f
RidwanS177 <85896066+RidwanS177@users.noreply.github.com>
2022-09-08 00:50:23 +0800

    Update nme.lua

--- nme.lua
+++ nme.lua
@@ -3,9 +3,11 @@ if Trial then
    Trial = false
 end
 
+--[[
 delayht = 105 -- DELAY HARVEST
 altdelayht = 60 -- UBAH JADI 0 JIKA SERVER BURIK
 delayplant = 75 -- DELAY PLANT
 delaypnb = 180 -- DELAY PNB
 delayplace = 100 -- DELAY PLACE
 delaytele = 10 --DELAY FINDPATH
+]]--
