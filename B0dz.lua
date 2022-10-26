057568eaec1e6fa0114adb57f6f6f81d9af7372d
RidwanS177 <85896066+RidwanS177@users.noreply.github.com>
2022-10-26 16:33:25 +0700

    Create B0dz.lua

---
+++ B0dz.lua
@@ -0,0 +1,35 @@
+Rdp = {
+    {nordp = 1, botname = "308MalesBgt", Status = "Nonaktif"},
+    {nordp = 1, botname = "316MalesBgt", Status = "Nonaktif"},
+    {nordp = 2, botname = "TizyRN1", Status = "Nonaktif"},
+    {nordp = 2, botname = "TizyRN10", Status = "Nonaktif"},
+    {nordp = 3, botname = "TizyRN13", Status = "Nonaktif"},
+    {nordp = 3, botname = "TizyRN15", Status = "Nonaktif"},
+    {nordp = 4, botname = "TizyRN60", Status = "Nonaktif"},
+    {nordp = 4, botname = "TizyRN62", Status = "Nonaktif"},
+    {nordp = 5, botname = "311MalesBgt", Status = "Nonaktif"},
+    {nordp = 5, botname = "312MalesBgt", Status = "Nonaktif"},
+    {nordp = 6, botname = "315MalesBgt", Status = "Nonaktif"},
+    {nordp = 6, botname = "TizyRN19", Status = "Nonaktif"}
+}
+
+ip_add = {
+    "192.168.0.1",
+    "192.168.0.2",
+    "192.168.0.3",
+    "192.168.0.4",
+    "192.168.0.5",
+    "192.168.0.6",
+    "192.168.0.7"
+}
+
+hookURL = "https://discord.com/api/webhooks/1034661496824803410/AguJfcVmvfzsQhk_iT9-hLCq7d-dvWkqfAurIsVm-X63GSAN98tce1jYFafkJ7xnaGHO"
+
+Teks_Deskripsi = "List Nick Para Babu"
+Teks_Footer = "Golek Duit Kanggo Masa Depan"
+
+Emote_Offline = "<:offline:982957367392088124>"
+Emote_Online = "<:online:982957403756716052>"
+
+menit = 5
+ShowIP = true
