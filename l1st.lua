1c81a61ce4f1d37dd7c5ee50b93a398e2ec0ae76
RidwanS177 <85896066+RidwanS177@users.noreply.github.com>
2022-10-28 11:23:50 +0700

    Update l1st.lua

--- l1st.lua
+++ l1st.lua
@@ -4,7 +4,7 @@ Member = {
     {iddc = "209222461357686795", Sandi = "XXX", Url = "https://raw.githubusercontent.com/iMashedGT/zczxczosadkdaskasdkmoasdkoadswasd/main/ododo"},
     {iddc = "1009508942117863557", Sandi = "XXX", Url = "https://raw.githubusercontent.com/Mamboy13/Offline-Online/main/Default_Github.lua.txt"},
     {iddc = "941902260923940885", Sandi = "tanz#721", Url = "https://raw.githubusercontent.com/Bintang278/tanzcekbot/main/Default_Github.lua"},
-    {iddc = "404191532149768193", Sandi = "XXX", Url = "https://raw.githubusercontent.com/petkem/bot-checker/main/bot-check"},
+    {iddc = "404191532149768193", Sandi = "PUTANGINABOBO", Url = "https://raw.githubusercontent.com/petkem/bot-checker/main/bot-check"},
     {iddc = "455384591050604547", Sandi = "XXX", Url = "https://raw.githubusercontent.com/Ayesssss/Bot/main/Check%20Bot.lua"},
     {iddc = "979781138660876328", Sandi = "267398", Url = "https://raw.githubusercontent.com/NizarRitwan/rdp-checkker/main/Default_Github.lua"},
     {iddc = "650576283998093322", Sandi = "2709", Url = "https://raw.githubusercontent.com/ZURBBB/cekbot/main/CEKBOT.lua"}
