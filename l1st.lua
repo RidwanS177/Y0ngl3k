042127098b13ef6db22b42ffea2df17069f75b35
RidwanS177 <85896066+RidwanS177@users.noreply.github.com>
2022-10-28 10:03:18 +0700

    Update l1st.lua

--- l1st.lua
+++ l1st.lua
@@ -1,5 +1,3 @@
 Member = {
     {iddc = "422670528483033088", Sandi = "1500", Url = "https://raw.githubusercontent.com/RidwanS177/Bot-Pantau/main/namabot.lua"}
 }
-
-CekMember(user_discord, Pin, JudulBot)
