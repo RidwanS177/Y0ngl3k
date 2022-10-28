550812f7b797f3a37e9e33989836999456b026fe
RidwanS177 <85896066+RidwanS177@users.noreply.github.com>
2022-10-28 09:59:16 +0700

    Update l1st.lua

--- l1st.lua
+++ l1st.lua
@@ -2,19 +2,4 @@ Member = {
     {iddc = "422670528483033088", Sandi = "1500", Url = "https://raw.githubusercontent.com/RidwanS177/Bot-Pantau/main/namabot.lua"}
 }
 
-function CekMember(idz, passz, namz)
-    NamaBuyer = namz
-    for _, v in pairs(Member) do
-        if idz == v.iddc then
-            if passz == v.Sandi then
-                LoadUrl = v.Url
-                Activz = true
-            else
-                odNotice("Sandi Anda Salah!")
-                sleep(30000)
-            end
-        end
-    end
-end
-
 CekMember(user_discord, Pin, JudulBot)
