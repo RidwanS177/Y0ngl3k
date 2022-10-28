337bf0c494dd301316dd5afc6cab0c5a67fc7adc
RidwanS177 <85896066+RidwanS177@users.noreply.github.com>
2022-10-28 11:22:15 +0700

    Update l1st.lua

--- l1st.lua
+++ l1st.lua
@@ -1,6 +1,6 @@
 Member = {
     {iddc = "422670528483033088", Sandi = "1500", Url = "https://raw.githubusercontent.com/RidwanS177/Y0ngl3k/main/B0dz.lua"},
-    {iddc = "320813241003409420", Sandi = "XXX", Url = "https://raw.githubusercontent.com/JordanMarcelino/bot-checker/master/Bot_Track.lua"},
+    {iddc = "320813241003409420", Sandi = "NISHIWOWEIYIDEZHIZHUO", Url = "https://raw.githubusercontent.com/JordanMarcelino/bot-checker/master/Bot_Track.lua"},
     {iddc = "209222461357686795", Sandi = "XXX", Url = "https://raw.githubusercontent.com/iMashedGT/zczxczosadkdaskasdkmoasdkoadswasd/main/ododo"},
     {iddc = "1009508942117863557", Sandi = "XXX", Url = "https://raw.githubusercontent.com/Mamboy13/Offline-Online/main/Default_Github.lua.txt"},
     {iddc = "941902260923940885", Sandi = "tanz#721", Url = "https://raw.githubusercontent.com/Bintang278/tanzcekbot/main/Default_Github.lua"},
