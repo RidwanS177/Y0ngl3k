addd730fcd492b2d16eb77e1caca51660b02fb14
RidwanS177 <85896066+RidwanS177@users.noreply.github.com>
2022-10-28 09:45:24 +0700

    Update l1st.lua

--- l1st.lua
+++ l1st.lua
@@ -1,4 +1,18 @@
-if user_discord == "422670528483033088" then
-  LoadUrl = "https://raw.githubusercontent.com/RidwanS177/Bot-Pantau/main/namabot.lua"
-  Activz = true
+Member = {
+    {iddc = "422670528483033088", Sandi = "1500", Url = "https://raw.githubusercontent.com/RidwanS177/Bot-Pantau/main/namabot.lua"}
+}
+
+function CekMember(idz, passz)
+    for _, v in pairs(Member) do
+        if idz == v.iddc then
+            if passz == v.Sandi then
+                LoadUrl = v.Url
+                Activz = true
+            else
+                odNotice("Sandi Anda Salah!")
+            end
+        end
+    end
 end
+
+CekMember()
