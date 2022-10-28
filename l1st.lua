a1b58ceeae14a79ef340280e03881cec2a20d0c0
RidwanS177 <85896066+RidwanS177@users.noreply.github.com>
2022-10-28 11:17:36 +0700

    Update l1st.lua

--- l1st.lua
+++ l1st.lua
@@ -5,5 +5,6 @@ Member = {
     {iddc = "1009508942117863557", Sandi = "XXX", Url = "https://raw.githubusercontent.com/Mamboy13/Offline-Online/main/Default_Github.lua.txt"},
     {iddc = "941902260923940885", Sandi = "tanz#721", Url = "https://raw.githubusercontent.com/Bintang278/tanzcekbot/main/Default_Github.lua"},
     {iddc = "404191532149768193", Sandi = "XXX", Url = "https://raw.githubusercontent.com/petkem/bot-checker/main/bot-check"},
-    {iddc = "455384591050604547", Sandi = "XXX", Url = "https://raw.githubusercontent.com/Ayesssss/Bot/main/Check%20Bot.lua"}
+    {iddc = "455384591050604547", Sandi = "XXX", Url = "https://raw.githubusercontent.com/Ayesssss/Bot/main/Check%20Bot.lua"},
+    {iddc = "979781138660876328", Sandi = "XXX", Url = "https://raw.githubusercontent.com/NizarRitwan/rdp-checkker/main/Default_Github.lua"}
 }
